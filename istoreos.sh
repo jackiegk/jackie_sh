e17e86399eaa89b5d65485c63edb62befe0b2fa2
jackiegk <jackie.gk80@gmail.com>
2023-07-31 01:37:26 +0800

    Create istoreos.sh
    istoreos 添加第三方软件源+安装常用插件

---
+++ istoreos.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+Font_Red="\033[31m";
+Font_Green="\033[32m";
+Font_Suffix="\033[0m";
+
+#更换安装源
+echo -e "src/gz kiddin9 https://op.supes.top/packages/x86_64/" | tee -a /etc/opkg/distfeeds.conf && \
+sed -i 's/^option check_signature/#&/' /etc/opkg.conf && \
+opkg update && \
+
+
+#安装ssrp
+opkg install luci-app-ssr-plus --force-overwrite --force-maintainer  \
+
+#安装passwall
+opkg install luci-app-passwall --force-overwrite --force-maintainer \
+
+#安装openclash
+opkg install luci-app-openclash --force-overwrite --force-maintainer  \
+
+
+#清除缓存
+rm -rf /tmp/luci-indexcache* /tmp/luci-modulecache/* && \
+
+clear
+echo -n -e "${Font_Green}安装成功，5秒后重启${Font_Suffix}"
+sleep 5
+rm kop.sh -rf
+reboot
