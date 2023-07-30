ad420f89019fda6e4a617ced537f4dd853fb27f0
jackiegk <jackie.gk80@gmail.com>
2023-07-31 02:25:56 +0800

    Update istoreos.sh

--- istoreos.sh
+++ istoreos.sh
@@ -5,29 +5,29 @@ Font_Green="\033[32m";
 Font_Suffix="\033[0m";
 
 #更换安装源
-echo -e "src/gz kiddin9 https://op.supes.top/packages/x86_64/" | tee -a /etc/opkg/distfeeds.conf && \
-sed -i 's/^option check_signature/#&/' /etc/opkg.conf && \
-opkg update && \
+echo -e "src/gz kiddin9 https://op.supes.top/packages/x86_64/" | tee -a /etc/opkg/distfeeds.conf && 
+sed -i 's/^option check_signature/#&/' /etc/opkg.conf && 
+opkg update && 
 
 
 #安装ssrp
-opkg install luci-app-ssr-plus --force-overwrite --force-maintainer  \
+opkg install luci-app-ssr-plus --force-overwrite --force-maintainer  
 
 #安装passwall
-opkg install luci-app-passwall --force-overwrite --force-maintainer \
+opkg install luci-app-passwall --force-overwrite --force-maintainer 
 
 #安装openclash
-opkg install luci-app-openclash --force-overwrite --force-maintainer  \
+opkg install luci-app-openclash --force-overwrite --force-maintainer  
 
 #安装NPS客户端
-opkg install luci-app-nps --force-overwrite --force-maintainer  \
+opkg install luci-app-nps --force-overwrite --force-maintainer 
 
 #安装zerotier
-opkg luci-app-zerotier  --force-overwrite --force-maintainer  \
+opkg luci-app-zerotier  --force-overwrite --force-maintainer  
 
 
 #清除缓存
-rm -rf /tmp/luci-indexcache* /tmp/luci-modulecache/* && \
+rm -rf /tmp/luci-indexcache* /tmp/luci-modulecache/* && 
 
 clear
 echo -n -e "${Font_Green}安装成功，5秒后重启${Font_Suffix}"
